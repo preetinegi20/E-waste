3cbb40f7f055092a8e0beebe5d9793a8e36a45b6
preetinegi20 <130657993+preetinegi20@users.noreply.github.com>
2023-04-23 22:54:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const ham=document.querySelector(".ham")
+const service=document.querySelector(".service")
+const sublist=document.querySelector(".subList")
+const ul=document.querySelector("nav .lists")
+service.addEventListener("mouseenter",()=>{
+    sublist.classList.toggle("sublistshow")
+})
+
+const lies=document.querySelectorAll("nav .lists li")
+ham.addEventListener("click",()=>{
+    ham.classList.toggle("rotate");
+        ul.classList.toggle("listshow");
+        lies.forEach(li=>{
+            li.classList.toggle("listanimate")
+        })
+})
+
+function reveal (){
+    const reveal=document.querySelectorAll(".reveal")
+    const revealside=document.querySelectorAll(".reveals")
+    const revealsideRight=document.querySelectorAll(".revealss")
+    let wdHT=window.innerHeight;
+     reveal.forEach(element=>{
+        let elementpos=element.getBoundingClientRect().top;
+        if(elementpos<=wdHT/1.3){
+        element.classList.add("reveal-active")
+        revealside.forEach(r=l=>{
+            l.classList.add("reveal-active-side")
+        })      
+        revealsideRight.forEach(r=>{
+            r.classList.add("reveal-active-side-right")
+        })  
+        }
+    })
+ 
+}
+document.addEventListener("DOMContentLoaded",reveal);
+window.addEventListener("scroll",reveal);
